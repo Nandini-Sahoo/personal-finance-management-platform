f16027b14b0f7feec72686426d15ba9cb888127b
Nandini Sahoo <leninandinisahoo123@gmail.com>
2026-05-06 22:07:10 +0530

    Change in report

--- backend/budget-func.php
+++ backend/budget-func.php
@@ -34,9 +34,9 @@ class BudgetFunctions {
                 FROM budget 
                 WHERE user_id = ?
                 UNION
-                SELECT DISTINCT DATE_FORMAT(expense_date, '%Y-%m') as month_year,
-                       DATE_FORMAT(expense_date, '%M %Y') as month_name
-                FROM expenses 
+                SELECT DISTINCT DATE_FORMAT(income_date, '%Y-%m') as month_year,
+                       DATE_FORMAT(income_date, '%M %Y') as month_name
+                FROM income 
                 WHERE user_id = ?
                 ORDER BY month_year DESC";
         
